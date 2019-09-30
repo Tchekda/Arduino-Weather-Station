0f3b3e522833f2fa68b2dcbca32f6474ef4ccfcf
Tchekda <contact@tchekda.fr>
2019-10-01 00:36:55 +0200

    Mainly finished

--- code/code.ino
+++ code/code.ino
@@ -7,7 +7,7 @@ int tempSensorPin = A0;
 int tempDigitalPin = 11;
 // Temperature default and constant values
 int ThermistorPin = A2;
-int Vo, T;
+int Vo, T, finalTemp, tempDiff = 47;
 float R1 = 10000; // value of R1 on board
 float logR2, R2;
 float c1 = 0.001129148, c2 = 0.000234125, c3 = 0.0000000876741; //steinhart-hart coeficients for thermistor
@@ -17,8 +17,9 @@ int analogToDegree(int Vo){
   R2 = R1 * (1023.0 / (float)Vo - 1.0); //calculate resistance on thermistor
   logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2)); // temperature in Kelvin
-  T = T - 230;
-  return T;
+  T = T - 273.15;
+  finalTemp = T + tempDiff;
+  return finalTemp;
 }
 
 
@@ -37,10 +38,12 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 // ------------ Bluetooth Module HC-05  ------------
 #include <SoftwareSerial.h>
-SoftwareSerial BTSerial(8, 7); 
+SoftwareSerial BTSerial(8, 9); 
+boolean BTconnected = false;
+int BTpin = 11;
 
-void buzzer(){
-  sinVal = (sin(0*(3.1412/180)));
+void buzzer(int toneHeight){
+  sinVal = (sin(toneHeight*(3.1412/180)));
   toneVal = 2000+(int(sinVal*1000));
   tone(buzzerPin, toneVal, 1000);
 }
@@ -59,6 +62,8 @@ void setup() {
   lcd.begin(16, 2);
   lcd.print("Weather Station");
 
+  Serial.println("Arduino is ready");
+
   BTSerial.begin(9600);
   
   delay(2000);
@@ -70,51 +75,57 @@ void loop() {
   // Disable both Leds at the beginning of the loop
   digitalWrite(blueLEDPin, LOW);
   digitalWrite(redLEDPin, LOW);
+
+  if ( digitalRead(BTpin)==HIGH && BTconnected == false)  { 
+    BTconnected = true;
+    Serial.println("Device connected");
+  }else if ( digitalRead(BTpin)==LOW && BTconnected == true)  { 
+    BTconnected = false;
+    Serial.println("Device disconnected");
+  }
   
   lcd.setCursor(0, 0);
-  /*if (digitalRead(tempDigitalPin) == LOW ||fixInput == 0){ // If the sensor does not feel anything
-    Serial.print("Temperature unavaible, recorded : ");
-    Serial.println(analogRead(tempSensorPin));
-    lcd.print("Temp false : ");
-    lcd.print(analogRead(tempSensorPin));
-    delay(1000); // Wait 1 more second
-  }else {*/
-    T = (int) analogToDegree(analogRead(ThermistorPin)); // Get Analog value and convert it to Celcius degrees
-    if (T <= 17){ // If too Cold
-      if (tempBuzzed == false){
-        buzzer();
-        tempBuzzed = true;
-      }
-      digitalWrite(blueLEDPin, HIGH);
-    }else if (T >= 25){ // If too Hot
-      if (tempBuzzed == false){
-        buzzer();
-        tempBuzzed = true;
-      }
-      digitalWrite(redLEDPin, HIGH);
+  analogToDegree(analogRead(ThermistorPin)); // Get Analog value and convert it to Celcius degrees
+  if (finalTemp <= 17){ // If too Cold
+    if (tempBuzzed == false){
+      buzzer(90);
+      tempBuzzed = true;
     }
-    Serial.print("Temperature = ");
-    Serial.println(T);
-    lcd.print("Temp: ");
-    lcd.print(T);
-    lcd.print(" C");
-  //}
+    digitalWrite(blueLEDPin, HIGH);
+  }else if (finalTemp >= 25){ // If too Hot
+    if (tempBuzzed == false){
+      buzzer(90);
+      tempBuzzed = true;
+    }
+    digitalWrite(redLEDPin, HIGH);
+  }
+  Serial.print("Temperature = ");
+  Serial.println(finalTemp);
+  lcd.print("Temp: ");
+  lcd.print(finalTemp);
+  lcd.print(" C");
 
   lightValue = analogRead(lightAnalogPin);
   Serial.print("Light = ");
   if (lightValue < 250 && lightBuzzed == false){ // If luminosity under 250 lux
-    buzzer();
+    buzzer(0);
     lightBuzzed = true;
-    //Serial.print("Has to Buzz : ");
   }else if (lightValue >= 250 && lightBuzzed == true){
     lightBuzzed = false;
-    //Serial.print("Had Buzzed : ");
   }
   Serial.println(lightValue);
   lcd.setCursor(0, 1);
   lcd.print("Light : ");
   lcd.print(lightValue);
   lcd.print(" lux");
+
+  if (BTconnected == true){
+    BTSerial.print(finalTemp);
+    BTSerial.print("&");
+    BTSerial.print(lightValue);
+    BTSerial.print("&");
+    Serial.println("BT sent");
+  }
 
   if (BTSerial.available() > 0){
       String inputString = "";
@@ -132,11 +143,16 @@ void loop() {
           {
               *valueCommand = 0;
               ++valueCommand;         
-                /*if(String(command) == "temp"){
-                  currentTemp = String(valueCommand).toInt() ;
-                  Serial.print("New current temp");
-                  Serial.println(currentTemp);
-                }else if(String(command) == "sensor"){
+                if(String(command) == "temp"){
+                  int requestTemp = String(valueCommand).toInt();
+                  tempDiff = requestTemp - T ;
+                  Serial.print("New temp diff : ");
+                  Serial.println(tempDiff);
+                  Serial.print("Current Temp : ");
+                  Serial.println(T);
+                  Serial.print("Request Temp : ");
+                  Serial.println(requestTemp);
+                }/*else if(String(command) == "sensor"){
                   fixInput = String(valueCommand).toInt() ;
                   Serial.print("New fixed Input");
                   Serial.println(fixInput);
@@ -146,5 +162,5 @@ void loop() {
       }
   }
   
-  delay(1000); // Wait 1 second between each loop
+  delay(200); // Wait 1 second between each loop
 }
